b340e9483ed20e2960a1d7b07974c67e39d30f1d
Johnathan Hoyt <johnathanhoyt@Gabriellas-MacBook-Pro.local>
2020-11-14 20:30:56 -0500

    created working endpoints to interact with db

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 const express = require('express');
 const server = express();
-
+const db = require('./data/db-config.js');
 server.use(express.json());
 
+server.get('/', (req, res) => {
+    db('cars')
+        .then(cars => {
+            res.status(200).json(cars);
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(500).json({message: 'There was an issue getting the cars from the server'});
+        })
+})
 
+server.post('/', (req, res) => {
+    const body = req.body;
+    db('cars').insert(body)
+        .then(newCar => {
+            res.status(201).json(newCar)
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(500).json({message: 'There was an issue saving the car to the database'});
+        })
+})
 
 module.exports = server;
